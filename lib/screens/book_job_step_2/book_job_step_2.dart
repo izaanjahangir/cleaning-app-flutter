5815d91b013bb5a696706cfa2b295ea6dd5bc34f
izaanjahangir <izaanjahangir2@gmail.com>
2021-04-08 13:52:55 +0500

    UI modification

--- lib/screens/book_job_step_2/book_job_step_2.dart
+++ lib/screens/book_job_step_2/book_job_step_2.dart
@@ -18,78 +18,100 @@ class _BookJobStep2State extends State<BookJobStep2> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        drawer: AppDrawer(),
-        body: SafeArea(
-          child: Column(
-            children: [
-              AppHeader(),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Step 2 of 3",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: ThemeColors.black,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text("Time Details",
-                  style: TextStyle(
-                      fontSize: 26,
-                      color: ThemeColors.blue,
-                      fontWeight: FontWeight.bold)),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        child: PhysicalModel(
-                          color: ThemeColors.black,
-                          elevation: 3,
-                          child: Container(
-                            color: ThemeColors.white,
-                            height: 150,
-                            child: CupertinoDatePicker(
-                                onDateTimeChanged: (DateTime newDate) {}),
+      child: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus &&
+              currentFocus.focusedChild != null) {
+            FocusManager.instance.primaryFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+          drawer: AppDrawer(),
+          body: SafeArea(
+            child: Column(
+              children: [
+                AppHeader(),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 10,
                           ),
-                        ),
+                          Text(
+                            "Step 2 of 3",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: ThemeColors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text("Time Details",
+                              style: TextStyle(
+                                  fontSize: 26,
+                                  color: ThemeColors.blue,
+                                  fontWeight: FontWeight.bold)),
+                          Container(
+                            margin: const EdgeInsets.only(top: 20),
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Column(
+                              children: [
+                                Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  child: PhysicalModel(
+                                    color: ThemeColors.black,
+                                    elevation: 3,
+                                    child: Container(
+                                      color: ThemeColors.white,
+                                      height: 150,
+                                      child: CupertinoDatePicker(
+                                          onDateTimeChanged:
+                                              (DateTime newDate) {}),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Instructions",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Container(
+                                    margin: const EdgeInsets.only(top: 10),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20),
+                                    child: TextArea())
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "Instructions",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      Container(
-                          margin: const EdgeInsets.only(top: 10),
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: TextArea())
-                    ],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(bottom: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        label: "Back"),
-                    Button(onPressed: () {}, label: "Next")
-                  ],
-                ),
-              )
-            ],
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  margin: const EdgeInsets.only(bottom: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Button(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          label: "Back"),
+                      Button(onPressed: () {}, label: "Next")
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
