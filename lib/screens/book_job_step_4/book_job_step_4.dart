22b75672d6f8fdc8348f7dca3687e654204bb24e
izaanjahangir <izaanjahangir2@gmail.com>
2021-04-09 23:49:53 +0500

    small fixes

--- lib/screens/book_job_step_4/book_job_step_4.dart
+++ lib/screens/book_job_step_4/book_job_step_4.dart
@@ -1,13 +1,16 @@
 import 'package:cleaning_app/components/app_drawer/app_drawer.dart';
 import 'package:cleaning_app/components/button/button.dart';
 import 'package:cleaning_app/config/theme_colors.dart';
+import 'package:cleaning_app/screens/add_card/add_card.dart';
+import 'package:cleaning_app/screens/book_job_step_4/details.dart';
+import 'package:cleaning_app/screens/book_job_step_4/select_card_section.dart';
+// import 'package:cleaning_app/screens/book_job_step_4/book_job_step_4.dart';
+import 'package:cleaning_app/utils/helpers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:async';
-import "package:cleaning_app/utils/location.dart";
 
 import "package:cleaning_app/components/app_header/app_header.dart";
+import 'package:touchable_opacity/touchable_opacity.dart';
 
 class BookJobStep4 extends StatefulWidget {
   static String screenName = "book-job-step-4";
@@ -17,187 +20,145 @@ class BookJobStep4 extends StatefulWidget {
 }
 
 class _BookJobStep4State extends State<BookJobStep4> {
-  Completer<GoogleMapController> _controller = Completer();
-  Set<Marker> markers = {};
+  List<Map> cards = [
+    {"id": 0, "number": "4111111111111111", "holderName": "izaan jahangir"},
+    {"id": 1, "number": "4111111111111111", "holderName": "izaan jahangir"},
+    {"id": 2, "number": "4111111111111111", "holderName": "izaan jahangir"},
+  ];
+  Map selectedCard;
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  Future<void> goToLocation(double latitde, double longitude,
-      {bool shouldNotZoom = false}) async {
-    final GoogleMapController controller = await _controller.future;
-    double zoom;
-
-    if (shouldNotZoom) {
-      zoom = await controller.getZoomLevel();
-    } else {
-      zoom = 19.151926040649414;
-    }
-
-    CameraPosition cameraPosition = CameraPosition(
-        bearing: 192.8334901395799,
-        target: LatLng(latitde, longitude),
-        zoom: zoom);
-
-    controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-  }
-
-  @override
-  void initState() {
-    getCurrentLocation();
-
-    super.initState();
-  }
-
-  void getCurrentLocation() async {
-    try {
-      var p = await Location.determinePosition();
-      GoogleMapController controller;
-
-      if (_controller.isCompleted) {
-        controller = await _controller.future;
-      }
-
-      goToLocation(p.latitude, p.longitude);
-      Marker currentLocationMarker = Marker(
-          markerId: MarkerId("current-location-marker"),
-          infoWindow: InfoWindow(
-              title: "Your Location", snippet: "We will come at this location"),
-          position: LatLng(p.latitude, p.longitude));
-
-      setState(() {
-        markers.add(currentLocationMarker);
-      });
-
-      if (_controller != null) {
-        await Future.delayed(Duration(seconds: 1));
-        controller.showMarkerInfoWindow(MarkerId("current-location-marker"));
-      }
-    } catch (e) {
-      print("some error " + e.toString());
-    }
-  }
-
-  void centerMap() {
-    final Marker marker = markers.elementAt(0);
-    goToLocation(marker.position.latitude, marker.position.longitude);
+  Widget renderChipButton(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Material(
+        color: ThemeColors.blue,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).pushNamed(AddCard.screenName);
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Row(
+              children: [
+                Text("Add More",
+                    style: TextStyle(fontSize: 10, color: ThemeColors.white)),
+                TouchableOpacity(
+                    child: Icon(
+                  Icons.add,
+                  size: 14,
+                  color: ThemeColors.white,
+                ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-
     return SafeArea(
-      child: Scaffold(
-        drawer: AppDrawer(),
-        body: SafeArea(
-          child: Column(
-            children: [
-              AppHeader(),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Container(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "Step 4 of 4",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: ThemeColors.black,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text("Address Details",
+      child: GestureDetector(
+        onTap: () {
+          Helpers.dismissKeyboardOnTap(context);
+        },
+        child: Scaffold(
+          drawer: AppDrawer(),
+          body: SafeArea(
+            child: Column(
+              children: [
+                AppHeader(),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Step 4 of 4",
                             style: TextStyle(
-                                fontSize: 26,
-                                color: ThemeColors.blue,
-                                fontWeight: FontWeight.bold)),
-                        Container(
-                          width: double.infinity,
-                          margin: const EdgeInsets.only(top: 10),
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              SizedBox(
-                                height: height * 0.45,
-                                width: double.infinity,
-                                child: GoogleMap(
-                                  mapType: MapType.normal,
-                                  myLocationEnabled: true,
-                                  myLocationButtonEnabled: true,
-                                  initialCameraPosition: _kGooglePlex,
-                                  onTap: (LatLng coordinates) {
-                                    Marker currentLocationMarker = Marker(
-                                        markerId:
-                                            MarkerId("current-location-marker"),
-                                        infoWindow: InfoWindow(
-                                            title: "Your Location",
-                                            snippet:
-                                                "We will come at this location"),
-                                        position: LatLng(coordinates.latitude,
-                                            coordinates.longitude));
-
-                                    setState(() {
-                                      markers.clear();
-                                      markers.add(currentLocationMarker);
-                                    });
-
-                                    _controller.future
-                                        .then((GoogleMapController controller) {
-                                      controller.showMarkerInfoWindow(
-                                          MarkerId("current-location-marker"));
-                                    });
-                                  },
-                                  markers: markers,
-                                  onMapCreated:
-                                      (GoogleMapController controller) {
-                                    _controller.complete(controller);
-                                    print(markers.toString());
-                                  },
+                                fontSize: 20,
+                                color: ThemeColors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text("Pay Details",
+                              style: TextStyle(
+                                  fontSize: 26,
+                                  color: ThemeColors.blue,
+                                  fontWeight: FontWeight.bold)),
+                          Container(
+                            width: double.infinity,
+                            margin: const EdgeInsets.only(top: 10),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Details(),
+                                SizedBox(
+                                  height: 15,
                                 ),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 10),
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: Column(
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    Button(
-                                        onPressed: centerMap,
-                                        label: "Center the map")
+                                    Text(
+                                      "Pay via card",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    renderChipButton(context)
                                   ],
                                 ),
-                              )
-                            ],
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                SelectCardSection(
+                                  cards: cards,
+                                  selected: selectedCard,
+                                  onSelect: (Map selectedItem) {
+                                    setState(() {
+                                      selectedCard = selectedItem;
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(bottom: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        label: "Back"),
-                    Button(onPressed: () {}, label: "Next")
-                  ],
-                ),
-              )
-            ],
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  margin: const EdgeInsets.only(bottom: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Button(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          label: "Back"),
+                      Button(
+                          onPressed: () {
+                            // Navigator.of(context)
+                            //     .pushNamed(BookJobStep4.screenName);
+                          },
+                          label: "Next")
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
