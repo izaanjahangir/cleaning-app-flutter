7861e939af23eb488b06853764059b2efa58c411
izaanjahangir <izaanjahangir2@gmail.com>
2021-04-09 22:12:23 +0500

    working on google maps

--- lib/screens/book_job_step_4/book_job_step_4.dart
+++ lib/screens/book_job_step_4/book_job_step_4.dart
@@ -3,6 +3,8 @@ import 'package:cleaning_app/components/button/button.dart';
 import 'package:cleaning_app/config/theme_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 import "package:cleaning_app/components/app_header/app_header.dart";
 
@@ -14,6 +16,24 @@ class BookJobStep4 extends StatefulWidget {
 }
 
 class _BookJobStep4State extends State<BookJobStep4> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -50,7 +70,20 @@ class _BookJobStep4State extends State<BookJobStep4> {
                               vertical: 10, horizontal: 20),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [],
+                            children: [
+                              SizedBox(
+                                height: 200,
+                                width: double.infinity,
+                                child: GoogleMap(
+                                  mapType: MapType.hybrid,
+                                  initialCameraPosition: _kGooglePlex,
+                                  onMapCreated:
+                                      (GoogleMapController controller) {
+                                    _controller.complete(controller);
+                                  },
+                                ),
+                              )
+                            ],
                           ),
                         ),
                       ],
